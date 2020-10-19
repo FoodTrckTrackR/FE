e1747d62be370a03e9029c20ef58be23a3cc2c32
corey-power <CPower1248@gmail.com>
2020-10-19 13:42:47 -0400

    Initial commit, Installed npm, Axios and React Router

---
+++ food-truck-trackr/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Food Truck TrackR</h1>
+      <p>--landing page--</p>
+    </div>
+  );
+}
