d626ae6f8c5bc0cc4a067190f2c10e911555e341
corey-power <CPower1248@gmail.com>
2020-10-19 17:13:43 -0400

    Skeleton SignUp, added design and routes (no form functionality)

--- food-truck-trackr/src/App.js
+++ food-truck-trackr/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Switch, Route, Link} from "react-router-dom"
-import './App.css';
+import './App.css'
 import logo from "./Images/food-truck-trackr_logo.png"
 import SignUp from "./Components/SignUp"
 import LogIn from "./Components/LogIn"
@@ -8,7 +8,7 @@ import LogIn from "./Components/LogIn"
 export default function App() {
   return (
     <div className="App">
-      <div className="nav">
+      <div className="nav-top">
         <Link to="/">Home</Link>
         <Link to="SignUp">Sign-up</Link>
         <Link to="LogIn">Log-in</Link>
@@ -25,16 +25,16 @@ export default function App() {
 
         <Route path="/">
           <div className="title-container">
-            <img src={logo} alt="Logo" />
+              <img src={logo} alt="Logo" />
             <h1>Food Truck<br></br>TrackR</h1>
           </div>
         </Route>
       </Switch>
-      
-      <div className="nav">
+
+      <div className="nav-bottom">
         <Link to="/">Home</Link>
-        <Link to="Sign-up">Sign-up</Link>
-        <Link to="Log-in">Log-in</Link>
+        <Link to="SignUp">Sign-up</Link>
+        <Link to="LogIn">Log-in</Link>
       </div>
     </div>
   );
