5d1db1726430472afd123550faa5dfd7b836c8e9
JRIGS <raczkowskijonathan@gmail.com>
2020-10-20 16:34:53 -0400

    Added Redux store and Reducer.js

--- food-truck-trackr/src/index.js
+++ food-truck-trackr/src/index.js
@@ -4,4 +4,22 @@ import {BrowserRouter as Router} from "react-router-dom"
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<Router><App /></Router>,document.getElementById('root'));
+//importing redux provider
+import { Provider } from 'react-redux';
+
+import 'bulma/css/bulma.css';
+import './styles.scss';
+
+//importing our createStore & reducer
+import { createStore } from 'redux';
+import { reducer } from './reducer/index';
+//creating store which calls the reducer
+const store = createStore(reducer);
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(<Provider store={store}>
+            <Router>
+                <App />
+            </Router>
+    </Provider>, rootElement)
